8843a4f219722000f737e37b1edb8920792576f2
treastrain / Tanaka Ryoga <tr@tret.jp>
2025-12-17 01:14:51 +0900

    Add payload argument parsing for team and individual

--- Sources/AppStoreConnectTool/AppStoreConnectTool.swift
+++ Sources/AppStoreConnectTool/AppStoreConnectTool.swift
@@ -9,3 +9,47 @@ import Foundation
 
 public enum AppStoreConnectTool {
 }
+
+extension AppStoreConnectTool {
+    public enum Payload: Codable, Sendable {
+        case team(Team)
+        case individual(Individual)
+    }
+}
+
+extension AppStoreConnectTool.Payload {
+    public struct Team: Codable, Sendable {
+        public var issuerID: String
+        public var privateKeyID: String
+        public var privateKey: String
+        public var expirationDuration: TimeInterval?
+
+        public init(
+            issuerID: String,
+            privateKeyID: String,
+            privateKey: String,
+            expirationDuration: TimeInterval? = nil
+        ) {
+            self.issuerID = issuerID
+            self.privateKeyID = privateKeyID
+            self.privateKey = privateKey
+            self.expirationDuration = expirationDuration
+        }
+    }
+
+    public struct Individual: Codable, Sendable {
+        public var individualPrivateKeyID: String
+        public var individualPrivateKey: String
+        public var expirationDuration: TimeInterval?
+
+        public init(
+            individualPrivateKeyID: String,
+            individualPrivateKey: String,
+            expirationDuration: TimeInterval? = nil
+        ) {
+            self.individualPrivateKeyID = individualPrivateKeyID
+            self.individualPrivateKey = individualPrivateKey
+            self.expirationDuration = expirationDuration
+        }
+    }
+}
--- Sources/asctool/ASCTool.swift
+++ Sources/asctool/ASCTool.swift
@@ -12,6 +12,108 @@ import Foundation
 @main
 struct ASCTool: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
-        commandName: "\(Self.self)".lowercased()
+        commandName: "\(Self.self)".lowercased(),
+        usage: """
+            asctool <subcommand> --issuer-id <issuer-id> --private-key-id <private-key-id> --private-key <private-key> [--expiration-duration <expiration-duration>]
+            asctool <subcommand> --individual-private-key-id <individual-private-key-id> --individual-private-key <individual-private-key> [--expiration-duration <expiration-duration>]
+            """,
+        subcommands: [
+        ]
     )
+}
+
+extension ASCTool {
+    struct Arguments: ParsableArguments {
+        @OptionGroup
+        var team: Team
+
+        @OptionGroup
+        var individual: Individual
+
+        @Option(help: "The token's expiration time in Unix epoch time.")
+        var expirationDuration: TimeInterval?
+    }
+}
+
+extension ASCTool.Arguments {
+    struct Team: ParsableArguments {
+        @Option(help: "Your issuer ID from the API Keys page in App Store Connect.")
+        var issuerID: String?
+
+        @Option(help: "Your private key ID from App Store Connect.")
+        var privateKeyID: String?
+
+        @Option(help: "Your private key from App Store Connect.")
+        var privateKey: String?
+
+        var isValid: Bool {
+            issuerID != nil && privateKeyID != nil && privateKey != nil
+        }
+    }
+}
+
+extension ASCTool.Arguments {
+    struct Individual: ParsableArguments {
+        @Option(help: "Your private key ID from App Store Connect.")
+        var individualPrivateKeyID: String?
+
+        @Option(help: "Your private key from App Store Connect.")
+        var individualPrivateKey: String?
+
+        var isValid: Bool {
+            individualPrivateKeyID != nil && individualPrivateKey != nil
+        }
+    }
+}
+
+extension AppStoreConnectTool.Payload {
+    init?(from arguments: ASCTool.Arguments) throws {
+        switch (arguments.team.isValid, arguments.individual.isValid) {
+        case (true, true):
+            throw ValidationError("Only one of team or individual arguments must be provided.")
+        case (true, false):
+            self = try .team(Team(from: arguments.team, expirationDuration: arguments.expirationDuration))
+        case (false, true):
+            self = try .individual(Individual(from: arguments.individual, expirationDuration: arguments.expirationDuration))
+        case (false, false):
+            return nil
+        }
+    }
+}
+
+extension AppStoreConnectTool.Payload.Team {
+    init(from arguments: ASCTool.Arguments.Team, expirationDuration: TimeInterval?) throws {
+        guard let issuerID = arguments.issuerID,
+            let privateKeyID = arguments.privateKeyID,
+            let privateKey = arguments.privateKey,
+            !issuerID.isEmpty,
+            !privateKeyID.isEmpty,
+            !privateKey.isEmpty
+        else {
+            throw ValidationError("Team arguments are incomplete.")
+        }
+        self = AppStoreConnectTool.Payload.Team(
+            issuerID: issuerID,
+            privateKeyID: privateKeyID,
+            privateKey: privateKey,
+            expirationDuration: expirationDuration
+        )
+    }
+}
+
+extension AppStoreConnectTool.Payload.Individual {
+    init(from arguments: ASCTool.Arguments.Individual, expirationDuration: TimeInterval?) throws {
+        guard let individualPrivateKeyID = arguments.individualPrivateKeyID,
+            let individualPrivateKey = arguments.individualPrivateKey,
+            !individualPrivateKeyID.isEmpty,
+            !individualPrivateKey.isEmpty
+        else {
+            throw ValidationError("Individual arguments are incomplete.")
+        }
+        self = AppStoreConnectTool.Payload.Individual(
+            individualPrivateKeyID: individualPrivateKeyID,
+            individualPrivateKey: individualPrivateKey,
+            expirationDuration: expirationDuration
+        )
+    }
 }
